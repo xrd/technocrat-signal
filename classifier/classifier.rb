8421cc5f6ea873453f593138037e63bd51d5f356
Chris Dawson <xrdawson@gmail.com>
2016-05-23 16:43:39 -0700

    Add classifier and training, plus connect redis to brain

---
+++ classifier/classifier.rb
@@ -0,0 +1,28 @@
+require 'classifier'
+require 'sinatra'
+require "sinatra/json"
+
+set :bind, '0.0.0.0'
+
+b = Classifier::Bayes.new 'ruby', 'java'
+
+hello = <<"END"
+Hello, java (or ruby)
+POST /train/java?code=xxx
+POST /classify?code=xxx
+END
+
+get '/' do
+  hello
+end
+
+post '/train/:type' do
+  b.send "train_#{params['type']}", params['code']
+  json status: 'ok'
+end
+
+post '/classify' do
+  type = b.classify params['code']
+  json status: 'ok', type: type
+end
+
---
+++ classifier/train-ruby.sh
@@ -0,0 +1,1 @@
+curl --data 'code=puts "Hi,from+Ruby"' "http://192.168.99.100:4567/train/ruby"
